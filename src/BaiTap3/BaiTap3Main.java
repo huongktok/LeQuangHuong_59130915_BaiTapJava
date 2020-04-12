b23f62c628669f935a5e5d0023e628d67ef076a0
lehuo <lehuo@192.168.1.2>
2020-04-12 10:02:31 +0700

    Bai Tap Java

---
+++ src/BaiTap3/BaiTap3Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BaiTap3;
+
+/**
+ *
+ * @author lehuo
+ */
+public class BaiTap3Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
